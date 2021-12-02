fdbeb739232dcafcf08d3749ae1c4c5bf4931d95
megantrieu <meganatrieu@gmail.com>
2021-12-02 15:28:20 -0800

    m3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Recipe from './components/recipe'
+import NavBar from './components/navigation'
+import Home from './components/home';
+import RecipeDisplay from './components/recipeDisplay';
+import recipes from "./recipeData"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+          <NavBar/>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            {recipes.map(recipe =>
+              <Route exact path={recipe.link} element={
+                <Recipe 
+                  name={recipe.name} 
+                  desc={recipe.desc}
+                  image={recipe.recipeImage}
+                  ingredients={recipe.ingredients}
+                  steps={recipe.prep}
+                  size={recipe.size}
+                />
+              }
+              />
+            )};
+          </Routes>
+    </BrowserRouter>
     </div>
   );
 }
